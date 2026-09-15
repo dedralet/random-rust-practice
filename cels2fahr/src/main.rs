cfed4f937f57d1fc4344513238a45ee7c4c01830
dedralet <dedralet@student.21-school.ru>
2026-09-15 17:27:53 +0500

    change: code formatted using rustfmt

--- cels2fahr/src/main.rs
+++ cels2fahr/src/main.rs
@@ -2,22 +2,22 @@ use std::io;
 
 fn main() {
     println!("Celsius to Fahrenheit converter");
-    
-    loop{
+
+    loop {
         println!("Enter a value:");
 
         let mut input = String::new();
         io::stdin()
             .read_line(&mut input)
             .expect("Input is not received");
-        let input: f64 = match input.trim().parse(){
-            Ok (num) => num,
-            Err (_) => {
+        let input: f64 = match input.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
                 println!("Invalid input. Try again");
                 continue;
-            },
+            }
         };
-        let fahrenheit: f64 = input * 9./5. + 32.0;
+        let fahrenheit: f64 = input * 9. / 5. + 32.0;
         println!("{input} °C = {fahrenheit} °F");
         break;
     }
