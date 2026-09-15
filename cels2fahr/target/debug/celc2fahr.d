c2c3082cc58c53464331ffc7c144e6ebd9837096
dedralet <dedralet@student.21-school.ru>
2026-09-15 16:44:33 +0500

    simple temperature converter added

---
+++ cels2fahr/target/debug/celc2fahr.d
@@ -0,0 +1,1 @@
+/home/dedralet/rust/random-rust-practice/celc2fahr/target/debug/celc2fahr: /home/dedralet/rust/random-rust-practice/celc2fahr/src/main.rs
---
+++ cels2fahr/target/debug/deps/celc2fahr-3544d2b32f50bcaa.d
@@ -0,0 +1,5 @@
+/home/dedralet/rust/random-rust-practice/celc2fahr/target/debug/deps/celc2fahr-3544d2b32f50bcaa.d: src/main.rs
+
+/home/dedralet/rust/random-rust-practice/celc2fahr/target/debug/deps/celc2fahr-3544d2b32f50bcaa: src/main.rs
+
+src/main.rs:
