9ef1b11312b93c2d34c383e3eeadd90b77f6b022
dedralet <dedralet@student.21-school.ru>
2026-09-16 16:27:38 +0500

    first_word.rs has been changed to second_word

---
+++ 4_second_word/src/main.rs
@@ -0,0 +1,38 @@
+use std::io;
+
+fn second_word(s: &str) -> &str {
+    let stringus = s.as_bytes();
+    let mut first_space = 0;
+    let mut started = false;
+
+    for (i, &iter) in stringus.iter().enumerate() {
+        if started == false && iter != b' ' {
+            started = true;
+        }
+        else if started == true {
+            if iter == b' ' {
+                if first_space == 0 {
+                    first_space = i+1;
+                }
+                else {
+                    return &s[first_space..i];
+                }
+            }
+        }
+    }
+    "Input contains only one word!"
+}
+
+fn main(){
+    println!("Enter your input:");
+
+    let mut input = String::new();
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read input");
+
+    let second_word = second_word(&input);
+
+    println!("{second_word}");
+}
---
+++ 4_second_word/target/release/deps/second_word-308fba520cf0209f.d
@@ -0,0 +1,5 @@
+/home/dedralet/rust/random-rust-practice/4_second_word/target/release/deps/second_word-308fba520cf0209f.d: src/main.rs
+
+/home/dedralet/rust/random-rust-practice/4_second_word/target/release/deps/second_word-308fba520cf0209f: src/main.rs
+
+src/main.rs:
