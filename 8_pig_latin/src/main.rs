b9407f580fe7f45b46910cb34c051dd720c3f591
dedralet <dedralet@student.21-school.ru>
2026-09-23 23:57:20 +0500

    fix: switched to .chars() instead of using &str[]

--- 8_pig_latin/src/main.rs
+++ 8_pig_latin/src/main.rs
@@ -4,11 +4,19 @@ fn translation(input: String) -> String {
     let mut felina: String = String::new();
 
     for word in input.split_whitespace() {
-        let temp = word.chars();
-        match &temp.as_str()[0..1] {
-            "a" | "e" | "i" | "o" | "u" => felina.push_str(&(format!("{word}-hay"))),
-            _ => felina.push_str(&(format!("{&temp[1..temp.len()]}-{&temp[0..1]}ay"))),
+        match &word.chars().next() {
+            Some('a') | Some('e') | Some('i') | Some('o') | Some('u') => felina.push_str(&(format!("{word}-hay"))),
+            _ => {
+                let temp = &word[1..];
+                for letter in temp.chars() {
+                    felina.push(letter);
+                }
+                felina.push('-');
+                felina.push(word.chars().next().unwrap());
+                felina.push_str("ay");
+            }
         }
+
         felina.push(' ');
     }
 
