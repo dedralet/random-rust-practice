5920af50ae0f0ce1dfd7a6d326f2b2adb9a01c5b
dedralet <dedralet@student.21-school.ru>
2026-09-24 00:16:22 +0500

    refactor: code formatted using rustfmt

--- 8_pig_latin/src/main.rs
+++ 8_pig_latin/src/main.rs
@@ -5,7 +5,9 @@ fn translation(input: String) -> String {
 
     for word in input.split_whitespace() {
         match &word.chars().next() {
-            Some('a') | Some('e') | Some('i') | Some('o') | Some('u') => felina.push_str(&(format!("{word}-hay"))),
+            Some('a') | Some('e') | Some('i') | Some('o') | Some('u') => {
+                felina.push_str(&(format!("{word}-hay")))
+            }
             _ => {
                 let temp = &word[1..];
                 for letter in temp.chars() {
@@ -29,7 +31,7 @@ fn main() {
     io::stdin()
         .read_line(&mut input)
         .expect("Failed to receive the input. Exiting...");
-    
+
     let felina: String = translation(input);
 
     println!("Here is Pig Latin variation of your text: {}", felina);
